57b0e051d33d0f725573dfbfd92f3fe71874891e
surajamrutkar <amrutkarsuraj98@gmail.com>
2026-06-24 22:33:27 +0530

    Remove Firebase credentials

--- gitignore.dart
+++ gitignore.dart
@@ -1,17 +1,46 @@
-# Firebase
-android/app/google-services.json
-ios/Runner/GoogleService-Info.plist
-
 # Flutter
+
     .dart_tool/
-build/
     .flutter-plugins
     .flutter-plugins-dependencies
+    .packages
+    .pub/
+build/
 
 # Android
-android/.gradle
+
+android/.gradle/
 android/local.properties
+android/key.properties
+
+# Firebase
+
+android/app/google-services.json
+
+# iOS Firebase
+
+ios/Runner/GoogleService-Info.plist
 
 # iOS
-ios/Pods
-ios/.symlinks
+
+ios/Pods/
+ios/.symlinks/
+ios/Flutter/Flutter.framework
+ios/Flutter/App.framework
+
+# VS Code
+
+    .vscode/
+
+# Android Studio / IntelliJ
+
+    .idea/
+*.iml
+
+# macOS
+
+    .DS_Store
+
+# Logs
+
+*.log
